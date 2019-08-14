7f764266e89cf908f9135b5429ec9dcde68667f0
Jose Galaz <jdgalazm@gmail.com>
2019-08-14 00:05:18 -0400

    set initial texture with separate kernel

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const init =  () =>{
-    
+    const size = 512;
     const gpu = new GPU();
-    debugger;
-    const render = gpu.createKernel(function() {
-        this.color(1, 0, 0, 1);
+    const initialKernel = gpu.createKernel(function(){
+        return this.thread.x/512;
+    })
+    .setOutput([size,size]);
+
+    const render = gpu.createKernel(function(input) {
+        this.color(input[this.thread.x][this.thread.y], 0, 0, 1);
     })
     .setOutput([512, 512])
     .setGraphical(true);
     
-    render();
+    let texture = initialKernel();
+    render(texture);
     
     document.body.appendChild(render.canvas);
 
